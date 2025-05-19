321f78806386041c82a0ab9f5aa998ceff662a35
Jenushan <jenushan349@gmail.com>
2025-05-18 22:34:42 -0400

    Add GET /list route to fetch all show entries

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://localhost:8080"])
+CORS(app, origins=["https://localhost:3000"])
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///animes.db'
 
 db = SQLAlchemy(app) 
 
@@ -17,6 +20,26 @@ class Anime(db.Model):
 def home():
     return "AnimeShelf running"
 
+@app.route('/list', methods=['GET'])
+def get_list():
+    # Gets all the rows from the table 
+    show_list = Anime.query.all()
+
+    result = []
+    for show in show_list: 
+        result.append({
+            'id: ': show.id, 
+            'Title: ': show.title, 
+            'Genre: ': show.genre, 
+            'Score: ': show.score
+        })
+    
+    # converts the python code into JSON so the frontend can understand it
+    return jsonify(result), 200
+
+
 # Creates table defined on Anime(db.Model)
-with app.app_context():
-    db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)

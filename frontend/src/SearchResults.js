52f1c53c22110ade9f2819eff5f5e6bdccc766fc
Jenushan <jenushan349@gmail.com>
2025-06-06 14:07:22 -0400

    Add filter options to search results

--- frontend/src/SearchResults.js
+++ frontend/src/SearchResults.js
@@ -7,10 +7,11 @@ function SearchResults() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
   const [results, setResults] = useState([]);
+  const [filter, setFilter] = useState("All"); // stores which format user wants to see
 
   const qlQuery = `
     query ($search: String) {
-      Page(perPage: 25) {
+      Page(perPage: 100) {
         media(search: $search, type: ANIME) {
           id
           title {
@@ -59,42 +60,58 @@ function SearchResults() {
 
 
   return (
-    <div className="search-list">
-      {results.map((anime) => {
-        const scoreRank = anime.rankings?.find(
-          (r) => r.type === "RATED" && r.allTime
-        );
+    <div>
+      <h1 className="search-res">Search Results for: {query}</h1>
+      <div className="search-filter">
+        <button className="search-all" onClick={() => setFilter("All")}>All</button>
+        <button className="search-tv" onClick={() => setFilter("TV")}>TV</button>
+        <button className="search-movie" onClick={() => setFilter("MOVIE")}>Movie</button>
+        <button className="search-ona" onClick={() => setFilter("ONA")}>ONA</button>
+        <button className="search-special" onClick={() => setFilter("SPECIAL")}>Special</button>
+      </div>
 
-        return (
-          <div key={anime.id} className="anime-entry">
-            <img
-              className="search-image"
-              src={anime.coverImage.large}
-              alt={anime.title.english}
-            />
-            <div className="search-info">
-              <h3 className="search-title">
-                {anime.title?.english || anime.title?.romaji || anime.title?.native || "Title not found"}
-              </h3>
 
-              <div className="search-info-row">
-                <p className="search-format"><strong>{anime.format}</strong></p>
-                <p className="search-episode">({anime.episodes} episodes)</p>
+      <div className="search-list">
+        {results
+
+          .filter(anime => filter === "All" || anime.format === filter)
+          .map((anime) => {
+            const scoreRank = anime.rankings?.find(
+              (r) => r.type === "RATED" && r.allTime
+            );
+
+            return (
+
+              <div key={anime.id} className="anime-entry">
+                <img
+                  className="search-image"
+                  src={anime.coverImage.large}
+                  alt={anime.title.english}
+                />
+                <div className="search-info">
+                  <h3 className="search-title">
+                    {anime.title?.english || anime.title?.romaji || anime.title?.native || "Title not found"}
+                  </h3>
+
+                  <div className="search-info-row">
+                    <p className="search-format"><strong>{anime.format}</strong></p>
+                    <p className="search-episode">({anime.episodes} episodes)</p>
+                  </div>
+
+                  <div className="search-score-rank">
+                    <p className="search-scored">
+                      Scored: {(anime.averageScore / 10).toFixed(2) || "N/A"}
+                    </p>
+                    <p className="search-rank">Rank: {scoreRank ? `#${scoreRank.rank}` : "Not Ranked"}</p>
+                  </div>
+
+                  <button onClick={() => handleAddShelf(anime)} className="add-list-btn">Add to list</button>
+
+                </div>
               </div>
-
-              <div className="search-score-rank">
-                <p className="search-scored">
-                  Scored: {(anime.averageScore / 10).toFixed(2) || "N/A"}
-                </p>
-                <p className="search-rank">Rank: {scoreRank ? `#${scoreRank.rank}` : "Not Ranked"}</p>
-              </div>
-
-              <button onClick={() => handleAddShelf(anime)} className="add-list-btn">Add to list</button>
-
-            </div>
-          </div>
-        );
-      })}
+            );
+          })}
+      </div>
     </div>
   );
 }

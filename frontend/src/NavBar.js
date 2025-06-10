c0d20183a0f9cff4d7f7b9156a17cab498528759
Jenushan <jenushan349@gmail.com>
2025-06-10 13:10:13 -0400

    Add logo and login buttons to navigation bar

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import { useState } from 'react'
+import logo from './assets/anime-network-logo.png';
 
 
 function NavBar() {
@@ -15,25 +16,30 @@ function NavBar() {
   };
 
   return (
-    <nav className="navbar">
-      <div className="nav-left">
-
-        <Link to="/">Home</Link>
-        <Link to="/list">MyList</Link>
-        <Link to="/profile">Profile</Link>
-
+    <div className="navbar-wrapper">
+      <div className="logo-bar">
+        <img src={logo} className="logo-img" />
+        <button className="login-btn">Login</button>
+        <button className="signup-btn">Sign up</button>
       </div>
+      <nav className="navbar">
+        <div className="nav-left">
+          <Link to="/">Home</Link>
+          <Link to="/list">MyList</Link>
+          <Link to="/profile">Profile</Link>
+        </div>
 
-      <form onSubmit={submitHandle} className="nav-search">
-        <input
-          type="text"
-          placeholder="Search"
-          value={anime}
-          onChange={(e) => setAnime(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-    </nav>
+        <form onSubmit={submitHandle} className="nav-search">
+          <input
+            type="text"
+            placeholder="Search"
+            value={anime}
+            onChange={(e) => setAnime(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </nav>
+    </div>
   );
 }
 
